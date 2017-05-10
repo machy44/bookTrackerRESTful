e49effa231a9e496d72419e1499099cdbbba46f7
machy44 <machy44@gmail.com>
2017-05-10 13:58:08 +0200

    comments route finished

--- routes/books.js
+++ routes/books.js
@@ -23,39 +23,41 @@ booksRouter.route('/search')
   .get ( (req, res) => {
 
     const dataLength = Object.keys(req.query)
+
     if(dataLength.length===0) return res.status(200).json(dataLength); // return empty array when query doesnt have parameters
+
     let keysObject = {};
     if(req.query.hasOwnProperty('author')){
-      keysObject.author = {$like: `%${req.query.author}%` };
+      keysObject.author = { $like: `%${req.query.author}%` };
     };
     if(req.query.hasOwnProperty('year')){
-      keysObject.year  =   {$like: `%${req.query.year}%` };
+      keysObject.year  =   { $like: `%${req.query.year}%` };
     };
 
       Book.findAll({ where: keysObject, raw: true })
             .then( books=> {
-              //if query data dont exists in db
-              if(books.length===0) return res.status(404).json( {msg: 'Not found'} );
+              if(books.length===0) return res.status(404).json( {msg: 'Not found'} ); //if query data dont exists in db
 
-              const json = collectionJSON( req.headers.host, req.baseUrl, books, {query: false, template: true} );
+              const json = collectionJSON( req.headers.host, req.baseUrl, books, { query: false, template: true } );
               res.status(200).json( json );
-    }).catch(error => res.status(500).json( {msg: error.message, errors: error.errors}) );
+
+    }).catch( error => res.status(500).json( {msg: error.message, errors: error.errors}) );
   });
 
-// GET and POST collection books --> vracanje errora kada nema konekcije
+// GET and POST collection books --> returnin error where there is no connection
  booksRouter.route('/')
     .get( (req, res) => {
         Book.findAll( { limit:10, raw: true } ).then( books=>{
           const json = collectionJSON( req.headers.host, req.baseUrl, books, { query: true, template: true} );
           res.status(200).json( json );
-        }).catch(error => res.status(500).json( {msg: error.message, errors: error.errors}) );
+        }).catch( error => res.status(500).json( {msg: error.message, errors: error.errors}) );
   })
 
 // POST book --create new book and add location header to created resource // post on id must return error that u must create resource on collection
     .post( (req, res) => {
       Book.create(req.body).then( (book) => {
             res.status(201).append('Location', `books/${book.get('id')}`).json();//Location header get uri with new id of created book
-        }).catch( (error) => {
+        }).catch( error => {
             res.status(400).json({msg: error.message, constraint: error.name, errors: error.errors});
         });
   });
@@ -83,10 +85,10 @@ booksRouter.route('/:bookId')
     })
 // delete done
     .delete( (req, res) => {
-   Book.findById(req.params.bookId).then( deletedBook => {
-      if (!deletedBook)   return res.status(404).json( { msg: 'Not found' } ); //if book with input id doesnt exist return not found!
+   Book.findById(req.params.bookId).then( book => {
+      if (!book)   return res.status(404).json( { msg: 'Not found' } ); //if book with input id doesnt exist return not found!
       else {
-        Book.destroy( { where: { id: req.params.bookId } } ).then( book => {
+        Book.destroy( { where: { id: req.params.bookId } } ).then( deletedBook => {
              res.status(204).end(); // 204 without entity in response body
            }).catch( error => res.status(404).json( { msg: 'Not found' } ) );
       }
--- routes/comments.js
+++ routes/comments.js
@@ -8,12 +8,12 @@ const collectionJSON = require('../helpers/mediaTypeObject');
 // GET and POST on collection comments
 commentsRouter.route('/')
     .get( (req, res) => {
-        Comment.findAll( { where: { book_id: req.params.bookId }, limit:10, raw: true } ).then( comments => {
+        Comment.findAll( { where: { book_id: req.params.bookId }, limit:10, raw: true } ).then( comments => { // comments of book id defined in url
           const json = collectionJSON( req.headers.host, req.baseUrl, comments, { query: false, template: true } );
           res.status(200).json( json );
         }).catch(error => res.status(500).json( {msg: error.message, errors: error.errors}) );
     })
-    .post((req, res) => {
+    .post( (req, res) => {
     Book.findById(req.params.bookId).then( book => {
         if( !book ) return res.status(400).json( { msg: "book id is not valid" } );
         else {
@@ -22,26 +22,49 @@ commentsRouter.route('/')
               .catch(error => res.status(400).json( { msg: error } ));
         }
     })
-    .catch((error) => {
+    .catch( error => {
       res.status(500).json({msg: error.message, constraint: error.name, errors: error.errors});
     });
   });
-
 // GET, PATCH and DELETE on single comment
 commentsRouter.route('/:commentId')
     .get( (req, res) => { //200 else 404
-      Comment.findAll( { where: {  id: req.params.commentId , book_id: req.params.bookId }, raw: true } ).then( comment => {
+       Comment.findAll( { where: {  id: req.params.commentId , book_id: req.params.bookId }, raw: true } )
+      .then( comment => {
         const json = collectionJSON( req.headers.host, req.baseUrl, comment, { query: false, template: true } );
         res.status(200).json( json );
-      }).catch( error => res.status(404).json( { msg: "Not Found" } ) );
+      })
+      .catch( error => res.status(404).json( { msg: "Not Found" } ) );
   })
-
-    .patch((req, res) => {//200 else 404
-    res.status(200).json({ message: 'PUT this is comments!' });
-  })
-
-  .delete((req, res) => {//204 else 404
-    res.status(200).json({ message: 'DELETE this is comments!' });
+    .patch( (req, res) => {
+      Comment.findById(req.params.commentId).then( updateComment => {
+        if( !updateComment ) return res.status(400).json( { msg: "comment id is not valid" } );
+        else {
+              updateComment.updateAttributes( { text: req.body.text } ) // update only text in comments
+                .then( updatedComment => {
+                  updatedComment = updatedComment.dataValues; //catch only dataValues of object
+                  const json = collectionJSON( req.headers.host, req.baseUrl, [updatedComment], { query: false, template: true } );
+                  res.status(200).json( json );
+              })
+              .catch( error => res.status(400).json( { msg: error } ) );
+          }
+        })
+        .catch( error => {
+            res.status(500).json( { msg: error.message, constraint: error.name, errors: error.errors } );
+        });
+    })
+  .delete( (req, res) => {//204 else 404
+    Comment.findById(req.params.commentId).then( comment => {
+       if ( !comment )  return res.status(404).json( { msg: 'Not found' } ); //if book with input id doesnt exist return not found!
+         else {
+           Comment.destroy( { where: { id: req.params.commentId } } )
+           .then( deletedComment => { res.status(204).end();  })     // 204 without entity in response body
+           .catch( error => res.status(404).json( { msg: 'Not found' } ) );
+         }
+     })
+       .catch( error => {
+           res.status(500).json({msg: error.message, constraint: error.name, errors: error.errors});
+      });
   });
 
 module.exports = commentsRouter;
